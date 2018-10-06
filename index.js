f00ec83181691d4dab01ff64d947a0118f8e2373
Thomaz <luiz.thomaz.nas@gmail.com>
2018-10-05 21:42:16 -0400

    Version 1.0.0

--- index.js
+++ index.js
@@ -1,31 +1,90 @@
+//Date, currency and table format
+var dateFormat = require('dateformat');
+var currencyFormatter = require('currency-formatter');
+var tabular = require('tabular-json');
+
+function toMoney(num) {
+    return currencyFormatter.format(num, { code: 'USD' });
+}
+
+function isEmpty(obj) {
+    for(var key in obj) {
+        if(obj.hasOwnProperty(key))
+            return false;
+    }
+    return true;
+}
+
 // setup server
 // YOUR CODE
+var express = require('express');
+var app = express();
 
 // setup directory used to serve static files
 // YOUR CODE
+app.use(express.static('public'));
 
 // setup data store
 // YOUR CODE
+var low = require('lowdb');
+var fs = require('lowdb/adapters/FileSync');
+var adapter = new fs('db.json');
+var db = low(adapter);
 
 // required data store structure
 // YOUR CODE
-/*
-{ 
-    accounts:[
-        {name        : '',
-         email       : '',
-         balance     : 0,
-         password    : '',
-         transactions: []}
-    ] 
-}
-*/
+// init the data store
+db.defaults({ 
+                accounts:[
+/*                 {name        : '',
+                    email       : '',
+                    balance     : 0,
+                    password    : '',
+                    transactions: []} */
+                ] 
+            }
+           )   
+           .write();
+
 
 app.get('/account/create/:name/:email/:password', function (req, res) {
 
     // YOUR CODE
     // Create account route
     // return success or failure string
+
+    // Check if name already exists
+    var account;
+    var msg;
+
+    account = db.get('accounts').find({ name: req.params.name }).value();
+    if (!isEmpty(account)) {
+        msg = "FAILURE: An account with this NAME already exists !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    account = db.get('accounts').find({ email: req.params.email }).value();
+    if (!isEmpty(account)) {
+        msg = "FAILURE: An account with this EMAIL already exists !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    account = {
+        "name"          : req.params.name,
+        "email"         : req.params.email,
+        "balance"       : 0,
+        "password"      : req.params.password,
+        "transactions"  : null
+    };
+    db.get('accounts').push(account).write();
+    msg = "SUCCESS: Account " + req.params.name + " was created!";
+    console.log(msg);   
+    res.send(msg);
+
 });
 
 app.get('/account/login/:email/:password', function (req, res) {
@@ -34,36 +93,246 @@ app.get('/account/login/:email/:password', function (req, res) {
     // Login user - confirm credentials
     // If success, return account object    
     // If fail, return null
+    // YOUR CODE
+    // Create account route
+    // return success or failure string
+
+    // Check if name already exists
+    var account = db.get('accounts').find({ email: req.params.email }).value();
+    var msg;
+
+    if (isEmpty(account)) {
+        msg = "FAILURE: This account does NOT exists !";
+    }
+    else {
+        if (req.params.password != account.password) {
+            msg = "FAILURE: Invalid password !";
+        }    
+        else {
+            msg = "SUCCESS: " + account.name + " is now logged in.";
+        }
+    }    
+    console.log(msg);
+    res.send(msg);
+    return;
+
 });
 
-app.get('/account/get/:email', function (req, res) {
+app.get('/account/getbalance/:email', function (req, res) {
 
     // YOUR CODE
     // Return account based on email
+    // Check if name already exists
+    var account = db.get('accounts').find({ email: req.params.email }).value();
+    var msg;
+
+    if (isEmpty(account)) {
+            msg = "FAILURE: This account does NOT exists !";
+    }
+    else {
+        msg = "SUCCESS: Account " + account.name + "'s balance is " + toMoney(account.balance);
+    }
+
+    console.log(msg);
+    res.send(msg);
+    return;
 });
 
-app.get('/account/deposit/:email/:amount', function (req, res) {
+app.get('/account/deposit/:email/:description/:amount', function (req, res) {
 
     // YOUR CODE
     // Deposit amount for email
     // return success or failure string
+    // Check if name already exists
+    var account = db.get('accounts').find({ email: req.params.email }).value();
+    var amount,datetime,description;
+    var msg;
+
+    if ( isEmpty(account)) {
+            msg = "FAILURE: This account does NOT exists !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+    
+    amount = Number(req.params.amount);
+    if (isNaN(amount)) {
+        msg = "FAILURE: Amount needs to be a valid number !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    if (amount <=0) {
+        msg = "FAILURE: Amount needs to be greater than zero !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    description = req.params.description;
+    account.balance += amount; 
+    datetime = new Date();
+    datetime = dateFormat(datetime, "yyyy-mm-dd, h:MM:ss TT");
+    if ( isEmpty(account.transactions)) {
+        account.transactions = Array({DataTime: datetime , Description: description, Amount: toMoney(amount)});
+    } 
+    else {
+        account.transactions.push({DataTime: datetime , Description: description, Amount: toMoney(amount)});
+    }
+    db.write();
+    msg = "SUCCESS: In "+ datetime + ", account " + account.name + " was credited in " + toMoney(amount) +" and its total balance is " + toMoney(account.balance) + ".";
+    console.log(msg);
+    res.send(msg);
+    return;
 });
 
-app.get('/account/withdraw/:email/:amount', function (req, res) {
+app.get('/account/withdraw/:email/:description/:amount', function (req, res) {
 
     // YOUR CODE
     // Withdraw amount for email
     // return success or failure string
+    // Check if name already exists
+    var account = db.get('accounts').find({ email: req.params.email }).value();
+    var amount,datetime,description;
+    var msg;
+
+    if ( isEmpty(account)) {
+        msg = "FAILURE: This account does NOT exists !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    amount = Number(req.params.amount);
+    if (isNaN(amount)) {
+        msg = "FAILURE: Amount needs to be a valid number !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    if (amount <=0) {
+        msg = "FAILURE: Amount needs to be greater than zero !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    if (account.balance < amount) {
+        msg = "FAILURE: Withdraw amount EXCEEDS your funds !";
+        console.log(msg);
+        res.send(msg);
+        return;
+    }
+
+    description = req.params.description;
+    amount = amount * -1;
+    account.balance += amount; 
+    datetime = new Date();
+    datetime = dateFormat(datetime, "yyyy-mm-dd, h:MM:ss TT");
+    if ( isEmpty(account.transactions)) {
+            account.transactions = Array({DataTime: datetime , Description: description, Amount: toMoney(amount)});
+    } 
+    else {
+        account.transactions.push({DataTime: datetime , Description: description, Amount: toMoney(amount)});
+    }
+    db.write();
+    msg = "SUCCESS: In "+ datetime + ", account " + account.name + " was debited in " + toMoney(amount) +" and its total balance is " + toMoney(account.balance) + ".";
+    console.log(msg);
+    res.send(msg);
+    return;
+
 });
+
+function transactionsTablify(transactions) {
+    var html;
+    var opts = {
+        dot: "/",
+        separator: '  ',
+        dateFormatter: function(date) {return dateFormat(date, "yyyy-mm-dd, h:MM:ss TT");},
+        sort: [],
+        classes: {table: "table table-striped table-bordered"}
+      };
+       
+    html = tabular.html(transactions, opts);
+    
+    return html;
+}
 
 app.get('/account/transactions/:email', function (req, res) {
 
     // YOUR CODE
     // Return all transactions for account
+
+    // YOUR CODE
+    // Return account based on email
+    // Check if name already exists
+    var account = db.get('accounts').find({ email: req.params.email }).value();
+    var msg,msgTransactions;
+
+    if (isEmpty(account)) {
+        msg = "FAILURE: This account does NOT exists !";
+    }
+    else {
+        if ( isEmpty(account.transactions)) {
+            msgTransactions = '<br>'+ "No transactions found";
+        }
+        else {
+            // msgTransactions = '\n'+ JSON.stringify(account.transactions);  
+            msgTransactions = transactionsTablify(account.transactions);
+        }
+    }
+
+    msg = "SUCCESS: Account " + account.name + "'s transactions are listed below " + msgTransactions;
+    console.log(msg);
+    res.send(msg);
+    return;
+
 });
+
+function allDataTablify(accounts) {
+    var accountLength,transactionLength;
+    var html = "";
+    
+    accountLength = accounts.length;
+    for (var i=0; i < accountLength; i++) {
+        html += '<br>' + JSON.stringify(accounts[i].name) + " " + JSON.stringify(accounts[i].email)+ " " + JSON.stringify(accounts[i].password);
+        if ( ! isEmpty(accounts[i].transactions)) {
+            transactionLength = accounts[i].transactions.length;
+            for (var j=0; j < transactionLength; j++) {
+                html += '<br>' + "    "+ JSON.stringify(accounts[i].transactions[j]);
+            }
+        }
+    }
+
+    return html;
+
+}
 
 app.get('/account/all', function (req, res) {
 
     // YOUR CODE
     // Return data for all accounts
+    // Check if name already exists
+    var accounts = db.get('accounts').value();
+    var msg,msgAccounts;
+
+    if (isEmpty(accounts)) {
+        msgAccounts = '\n'+ "No accounts found";
+    }
+    else {
+        msgAccounts = '\n'+ allDataTablify(accounts);  
+    }
+
+    msg = "SUCCESS: All data " + msgAccounts;
+    console.log(msg);
+    res.send(msg);
+    return;
+
 });
+
+app.listen(8080,function () { 
+    console.log('Fiat bank (bad bank) http server is ready at 8080')
+});
+
--- public/modules/modules.js
+++ public/modules/modules.js
@@ -1,77 +1,537 @@
 var ui = {};
 
+// Variables that keep the current login
+var loggedInEmail = "";
+
 ui.navigation = `
     <!-- ------------- YOUR CODE: Navigation UI ------------- --> 
-`;
+  <a class="navbar-brand" href="#" onclick="defaultModule();">
+    <img src="/badbank_icon.png" width="50" height="50" class="d-inline-block align-center" alt=""  >
+     Fiat Bad Bank</a>
+ <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+   <span class="navbar-toggler-icon"></span>
+ </button>
 
-ui.createAccount = `
+ <div class="collapse navbar-collapse" id="navbarSupportedContent">
+   <ul class="navbar-nav mr-auto">
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadCreateAccountParam();">Create Account <!--<span class="sr-only">(current)</span> --> </a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadLoginParam();">Login</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadLogoutParam();">Logout</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadDepositParam();">Deposit</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadWithdrawParam();">Withdraw</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadTransactionsParam();">Transactions</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadBalanceParam();">Balance</a>
+     </li>
+     <li class="nav-item active">
+       <a class="nav-link" href="#" onclick="loadAllDataParam();">All Data</a>
+     </li>
+     <!-- <li class="nav-item dropdown">
+       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         Dropdown
+       </a>
+       <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+         <a class="dropdown-item" href="#">Action</a>
+         <a class="dropdown-item" href="#">Another action</a>
+         <div class="dropdown-divider"></div>
+         <a class="dropdown-item" href="#">Something else here</a>
+       </div>
+     </li>
+     <li class="nav-item">
+       <a class="nav-link disabled" href="#">Disabled</a>
+     </li> -->
+   </ul>
+   <form class="form-inline my-2 my-lg-0">
+      <div class="card bg-light mb-3" style="max-width: 22rem;" id="loggedin" >  </div>
+    </form>
+ </div>
+
+ `;
+
+ui.createAccount =  function (isResultMsg,msg) {
+
+    var innerHTML,conditionalHTML;
+
+    if (isResultMsg) {
+        conditionalHTML = msg;
+    } else {
+        conditionalHTML = `
+        Name: <br> <input type="input" id="addName" placeholder="" > <br>
+        Email: <br> <input type="input" id="addEmail" placeholder="" >  <br>
+        Password: <br> <input type="password" id="addPassword" placeholder="" >  <br>
+        Re-type your Password: <br> <input type="password" id="addPassword2" placeholder="" >  <br><br>
+        <button type="button" class="btn btn-info" onclick="loadCreateAccount()">Add Account</button>
+        `;
+    }
+        
+    innerHTML =`
     <!-- ------------- YOUR CODE: Create Account UI ------------- --> 
-`;
+    <div class="card-header">Fiat Bad Bank Create Account </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
 
-ui.login = `
-    <!-- ------------- YOUR CODE: Login UI ------------- --> 
-`;
+ui.login=  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
 
-ui.deposit = `
-    <!-- ------------- YOUR CODE: Deposit UI ------------- --> 
-`;
+    var innerHTML,conditionalHTML;
 
-ui.withdraw = `
-    <!-- ------------- YOUR CODE: Withdraw UI ------------- --> 
-`;
+    if (isResultMsg) {
+        conditionalHTML = msg;
+    } else {
+        conditionalHTML = `
+        Email: <br> <input type="input" id="loginEmail" value="" >  <br>
+        Password: <br> <input type="password" id="loginPassword" value="" >  <br><br>
+        <button type="button" class="btn btn-info" onclick="loadLogin()">Log in</button>
+        `;
+    }
+        
+    innerHTML =`
+    <!-- ------------- YOUR CODE: Create Account UI ------------- --> 
+    <div class="card-header">Fiat Bad Bank Login </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
 
-ui.transactions = `
+ui.logout=  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    conditionalHTML = msg;
+
+    innerHTML =`
+    <!-- ------------- YOUR CODE: Logout UI ------------- --> 
+    <div class="card-header">Fiat Bad Bank Logout </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
+
+ui.deposit =  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    if (isResultMsg) {
+        conditionalHTML = msg;
+    } else {
+        conditionalHTML = `
+        Description: <br> <input type="input" id="depositDescription" value="" >  <br>
+        Amount: <br> <input type="number" step="0.01" id="depositAmount" value="" >  <br>
+        <br> <button type="button" class="btn btn-info" onclick="loadDeposit()">Deposit</button>
+        `;
+    }
+        
+    innerHTML =`
+    <!-- ------------- YOUR CODE: Deposit UI  ------------- --> 
+    <div class="card-header">Fiat Bad Bank Deposit </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
+
+ui.withdraw  =  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    if (isResultMsg) {
+        conditionalHTML = msg;
+    } else {
+        conditionalHTML = `
+        Description: <br> <input type="input" id="withdrawDescription" value="" >  <br>
+        Amount: <br> <input type="number" step="0.01" id="withdrawAmount" value="" >  <br>
+        <br> <button type="button" class="btn btn-info" onclick="loadWithdraw()">Withdraw</button>
+        `;
+    }
+        
+    innerHTML =`
+    <!-- ------------- YOUR CODE: Withdraw UI  ------------- --> 
+    <div class="card-header">Fiat Bad Bank Withdraw </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
+
+ui.transactions =  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    conditionalHTML = msg;
+
+    innerHTML =`
     <!-- ------------- YOUR CODE: Transactions UI ------------- --> 
-`;
+    <div class="card-header">Fiat Bad Bank Transactions </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
 
-ui.balance = `
+ui.balance =  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    conditionalHTML = msg;
+
+    innerHTML =`
     <!-- ------------- YOUR CODE: Balance UI ------------- --> 
-`;
+    <div class="card-header">Fiat Bad Bank Balance </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
 
 ui.default = `
     <!-- ------------- YOUR CODE: Default UI ------------- --> 
+    <div class="card-header">Fiat Bad Bank Home </div>
+    <div class="card-body">
+      <h5 class="card-title">Welcome ! Use at your own risk</h5>
+      <img src="/badbank_icon.png" width="400" height="400" class="d-inline-block align-center" alt="">
+    </div>
 `;
 
-ui.allData = `
-    <!-- ------------- YOUR CODE: All Data UI ------------- --> 
-`;
+ui.allData =  function (isResultMsg,msg) {
+    var innerHTML,conditionalHTML;
+
+    var innerHTML,conditionalHTML;
+
+    conditionalHTML = msg;
+
+    innerHTML =`
+    <!-- ------------- YOUR CODE: All data UI ------------- --> 
+    <div class="card-header">Fiat Bad Bank All Accounts </div>
+    <div class="card-body">
+    ` +
+    conditionalHTML
+    + `
+    </div>
+    `;
+    return innerHTML;
+}
 
 var target     = document.getElementById('target');
 var navigation = document.getElementById('navigation');
 navigation.innerHTML += ui.navigation;
 
+var loadCreateAccountParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML
+    var obj = document.getElementById('loggedin');
+    obj.innerHTML = "";
+    loggedInEmail = "";
+    target.innerHTML = ui.createAccount(isResultMsg,"");
+}
 
 
 var loadCreateAccount = function(){
-    target.innerHTML = ui.createAccount;
+    var isResultMsg = 1; // it is not a result msg, therefore load the result HTML
+    var name = document.getElementById('addName').value;
+    var email = document.getElementById('addEmail').value;
+    var password = document.getElementById('addPassword').value;
+    var passwordReType = document.getElementById('addPassword2').value;
+    var url = "";
+    var loggedin;
+
+    if ((name == "") || (email == "") || (password == "") || (passwordReType == "")) {
+        alert("Required parameters can not be blank !");
+        return;
+    }
+    
+    if ((password != passwordReType)) {
+        alert("Passwords do NOT match !");
+        return;
+    }
+    
+    name = name.toUpperCase();
+    email = email.toUpperCase();
+    url = '/account/create/' + name + '/' + email + '/' + password;
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.createAccount(isResultMsg,res.text);
+            }
+
+        });  
 };
+
+var loadLoginParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML
+    target.innerHTML = ui.login(isResultMsg,"");
+}
 
 var loadLogin = function(){
-    target.innerHTML = ui.login;
+    var isResultMsg = 1; // it is not a result msg, therefore load the result HTML
+    var email = document.getElementById('loginEmail').value;
+    var password = document.getElementById('loginPassword').value;
+    var url = "";
+
+    if  ((email == "") || (password == "")) {
+        alert("Required parameters can not be blank !");
+        return;
+    }
+    
+    name = name.toUpperCase();
+    email = email.toUpperCase();
+    url = '/account/login/' + email + '/' + password;
+
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.createAccount(isResultMsg,res.text);
+                if ( res.text.includes("SUCCESS") ) {
+                    loggedInEmail = email;
+                    var obj = document.getElementById('loggedin');
+                    obj.innerHTML = email + " logged in";
+                }            
+            }
+
+        });
 };
+
+var loadLogoutParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML, if fact in this case this parameter doesn't matter
+    if (loggedInEmail == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    var msg = "SUCCESS: " + loggedInEmail + " is logged out";
+    var obj = document.getElementById('loggedin');
+    obj.innerHTML = "";
+    loggedInEmail = "";
+    target.innerHTML = ui.logout(isResultMsg,msg);
+}
+
+var loadDepositParam = function()  {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML
+    if (loggedInEmail == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    target.innerHTML = ui.deposit(isResultMsg,"");
+}
 
 var loadDeposit = function(){
-    target.innerHTML = ui.deposit;
+    var isResultMsg = 1; // it is not a result msg, therefore load the result HTML
+    var email = loggedInEmail;
+    var description = document.getElementById('depositDescription').value;
+    var amount = Number(document.getElementById('depositAmount').value);
+    var url = "";
+
+    if (email == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    
+    if (isNaN(amount)) {
+        alert("Amount needs to be a valid number !");
+        return;
+    }
+    
+    if (amount <=0) {
+        alert("Amount needs to be greater than zero !");
+        return;
+    }
+    if (description == "") {
+        description = "-";
+    }
+
+    url = '/account/deposit/' + email + '/' + description + '/' + amount ;
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.deposit(isResultMsg,res.text);
+            }
+
+        });
 };
+
+var loadWithdrawParam = function()  {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML
+    if (loggedInEmail == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    target.innerHTML = ui.withdraw(isResultMsg,"");
+}
 
 var loadWithdraw = function(){
-    target.innerHTML = ui.withdraw;
+    var isResultMsg = 1; // it is not a result msg, therefore load the result HTML
+    var email = loggedInEmail;
+    var description = document.getElementById('withdrawDescription').value;
+    var amount = Number(document.getElementById('withdrawAmount').value);
+    var url = "";
+
+    if (email == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    
+    if (isNaN(amount)) {
+        alert("Amount needs to be a valid number !");
+        return;
+    }
+    
+    if (amount <=0) {
+        alert("Amount needs to be greater than zero !");
+        return;
+    }
+    if (description == "") {
+        description = "-";
+    }
+
+    url = '/account/withdraw/' + email + '/' + description + '/' + amount ;
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.withdraw(isResultMsg,res.text);
+            }
+
+        });
 };
 
-var loadTransactions = function(){
-    target.innerHTML = ui.transactions;
-};
+var loadBalanceParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML, if fact in this case this parameter doesn't matter
+    var email = loggedInEmail;
+      var url = "";
 
-var loadBalance = function(){
-    target.innerHTML = ui.balance;
-};
+    if (email == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    
+    url = '/account/getbalance/' + email ;
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.balance(isResultMsg,res.text);
+            }
+
+        });
+
+}
+
+var loadTransactionsParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML, if fact in this case this parameter doesn't matter
+    var email = loggedInEmail;
+      var url = "";
+
+    if (email == "") {
+        alert("You must be logged in to perform this operation !");
+        return;
+    }
+    
+    url = '/account/transactions/' + email ;
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.transactions(isResultMsg,res.text);
+            }
+
+        });
+
+}
 
 var defaultModule = function(){
     target.innerHTML = ui.default;
 };
 
-var loadAllData = function(){
-    target.innerHTML = ui.allData;
+var loadAllDataParam = function() {
+    var isResultMsg = 0; // it is not a result msg, therefore load the parameters HTML, if fact in this case this parameter doesn't matter
+    var url = "";
+
+    url = '/account/all';
+    
+    superagent
+        .get(url)
+        .end(function(err, res){
+            if(err){
+                console.log(err);
+            }
+            else{
+                console.log(res);
+                target.innerHTML = ui.allData(isResultMsg,res.text);
+            }
+
+        });
 };
 
 defaultModule();
